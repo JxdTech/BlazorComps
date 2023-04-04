eb23b91cf09742ee171a7415293e5fea47f29155
Ramon D <ramon@jxdwebsolutions.com>
2023-04-03 23:46:10 -0600

    Create ServiceCollectionExtensions

---
+++ src/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorComps;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBlazorComps(this IServiceCollection services)
+        => services
+            .AddSingleton<IClassProvider, ClassProvider>()
+            .AddScoped<IBreakpointInterop, BreakpointInterop>();
+}
